2ef6b1a02fc6d3b0aaf9f9fdaf9999664e3e78b2
nickimbo <122122151+nickimbo@users.noreply.github.com>
2024-01-25 16:50:39 +0000

    modified:   src/Baser/Database.php 	modified:   src/Basic.php

--- src/Baser/Database.php
+++ src/Baser/Database.php
@@ -143,7 +143,7 @@ class Database implements IDatabase
      * ```
      *
      * @param array $options Connection options
-     * @return \Minichan\Database\Database
+     * @return Nickimbo\Utils\Baser\Database
      * @throws PDOException
      * @codeCoverageIgnore
      */
@@ -683,7 +683,7 @@ class Database implements IDatabase
         if (!preg_match('/^[\p{L}_][\p{L}\p{N}@$#\-_]*(\.?[\p{L}_][\p{L}\p{N}@$#\-_]*)?$/u', $column)) {
             throw new InvalidArgumentException("Incorrect column name: {$column}.");
         }
-
+        if($column[-1] === '`' && $column[0] === '`') return $column;
         return strpos($column, '.') !== false ?
             '"' . $this->prefix . str_replace('.', '"."', $column) . '"' :
             '"' . $column . '"';
@@ -2199,7 +2199,6 @@ class Database implements IDatabase
         }
 
         $isSingle = (is_string($column) && $column !== '*');
-        die($this->selectContext($table, $map, $join, $columns, $where));
         $query = $this->exec($this->selectContext($table, $map, $join, $columns, $where), $map);
 
         if (!$this->statement) {
--- src/Basic.php
+++ src/Basic.php
@@ -8,6 +8,9 @@ define('UTILS_CURRENT_URL', (
     ($_SERVER['HTTP_HOST'] ?? 'localhost') . 
     ($_SERVER['REQUEST_URI'] ?? '/')
 ));
+
+define('FALSE_PARSE', 0x4227);
+
 function castString($delimiter,$escaper,$text){$d=preg_quote($delimiter,"~");$e=preg_quote($escaper,"~");$tokens=preg_split('~'.$e.'('.$e.'|'.$d.')(*SKIP)(*FAIL)|(?<='.$d.')~',$text,-1,PREG_SPLIT_NO_EMPTY);$escaperReplacement=str_replace(['\\','$'],['\\\\','\\$'],$escaper);$delimiterReplacement=str_replace(['\\','$'],['\\\\','\\$'],$delimiter);return implode(preg_replace(['~\\\\.(*SKIP)(*FAIL)|'.($escaper.$delimiter).'~s','~'.$e.$d.'~'],['%s',$delimiterReplacement],$tokens));}
 class Basic {
     static public function String(string|array $haystack, string|number|bool ...$Args): string|null {
@@ -74,16 +77,55 @@ class Basic {
         }
         return $Args[0];
     }
-    static public function Parse(mixed $haystack, string $forceType = 'json'): array {
-        $T = in_array($forceType, ['json', 'object', 'boolean', 'integer', 'double', 'string', 'array', 'object', 'null', 'unknown']) ? ($forceType === 'json' ? 'object' : $forceType) : 'object';
-        $P;
+    static public function Parse(mixed $haystack, string $forceType = 'json'): mixed {
+        $T = in_array($forceType, ['json', 'object', 'boolean', 'integer', 'double', 'string', 'array', 'null', 'unknown']) ? ($forceType === 'json' ? 'object' : ($forceType === 'unknown' ? 'unknown type' : $forceType)) : 'object';
+        $O = strtolower(strval(gettype($haystack)));
+
+        if($T === $O) return $haystack;
+
         switch($T):
             case 'object':
-            if($forceType === 'json') {
-                
-            }
-            break;
+                if($forceType === 'json') return json_validate($haystack) ? json_decode($haystack) : FALSE_PARSE;
+                return (object)$haystack;
+            case 'boolean':
+                return boolval($haystack);
+            case 'integer':
+                return intval($haystack);
+            case 'double':
+                return doubleval($haystack);
+            case 'string':
+                return strval($haystack);
+            case 'array':
+                return (array)$haystack;
+            case 'null':
+                return is_null($haystack) ? $haystack : FALSE_PARSE;
+            case 'unknown type':
+                return 'unknown';
         endswitch;
+    }
+}
+
+
+class Parse {
+    protected $type;
+
+    protected $var;
+
+    protected $parsed;
+
+    function __construct(mixed $haystack, string $forceType = 'json') {
+        $this->type = strtolower(strval(gettype($haystack)));
+        $this->var = $haystack;
+        $this->parsed = Basic::Parse($haystack, $forceType);
+    }
+    public function get(): mixed {
+        return $this->parsed;
+    }
+    public function type(): string {
+        return $this->type;
+    }
+    public function var(): mixed {
+        return $this->var;
     }
 }
 ?>
